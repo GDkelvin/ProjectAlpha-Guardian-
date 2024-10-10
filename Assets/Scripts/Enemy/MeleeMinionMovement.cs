d7383092ca480bc22bfff6169c7a53efbd423e72
Bao <lhquocbao1703@gmail.com>
2024-10-10 14:29:59 +0700

    Added tower script for hp

---
+++ Assets/Scripts/Enemy/MeleeMinionMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MeleeMinionMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Animator anim;
+    
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    public void Run()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        anim.SetTrigger("run");
+    }
+}
---
+++ Assets/Scripts/Tower/TowerStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerStats : MonoBehaviour
+{ 
+    [SerializeField] private float hp;//Max HP
+    [SerializeField] private Image hpBar; //Drag Hp image to this in editor
+    private float currentHP;
+    
+
+    void Start()
+    {
+        currentHP = hp;
+    }
+
+    public void UpdateHP(float amount)
+    {
+        currentHP += amount;
+        currentHP = Mathf.Clamp(currentHP, 0f, hp); //When update hp, this wont make hp overflow the max HP, ex: when healing
+        UpdateHPBar();
+    }
+
+   
+    public void UpdateHPBar()
+    {
+        float targetFillAmount = currentHP / hp;
+        hpBar.fillAmount = targetFillAmount;
+    }
+}
